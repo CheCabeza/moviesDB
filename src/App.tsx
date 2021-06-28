3d3c80f81e1cfef633a35eb070a02856be9f4940
CheCabeza <chemartinc@gmail.com>
2021-06-28 11:27:28 +0200

    Import header into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>hola</h1>
     </div>
   );
