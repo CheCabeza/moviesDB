fbdd686358942e3a57bf72589571957751a77dfe
CheCabeza <chemartinc@gmail.com>
2021-06-30 10:33:23 +0200

    Create environment variables to hide api key

--- src/redux/actions/actionCreator.ts
+++ src/redux/actions/actionCreator.ts
@@ -5,7 +5,7 @@ import actionTypes from './actionTypes';
 export function loadMovies() {
   return async (dispatch: Dispatch) => {
     try {
-      const { data } = await axios('https://api.themoviedb.org/3/trending/movie/week?api_key=fd4391ab9e4ed087a42c2c2e4329b1d4');
+      const { data } = await axios(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`);
       dispatch({
         type: actionTypes.LOAD_MOVIES,
         movies: data,
@@ -21,7 +21,7 @@ export function loadMovies() {
 export function loadTv() {
   return async (dispatch: Dispatch) => {
     try {
-      const { data } = await axios('https://api.themoviedb.org/3/trending/tv/week?api_key=fd4391ab9e4ed087a42c2c2e4329b1d4');
+      const { data } = await axios(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_API_KEY}`);
       dispatch({
         type: actionTypes.LOAD_TVSHOWS,
         tvShows: data,
