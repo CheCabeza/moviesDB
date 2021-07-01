356cf4de82cf570b36abd42d7bc9465b10661324
CheCabeza <chemartinc@gmail.com>
2021-07-01 10:01:50 +0200

    Change some styles in Header component icons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,11 +12,11 @@ export default function Header() {
         <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg" alt="TMDB logo" className="header__logo" />
       </header>
       <div>
-        <Link to="/">
-          <ImFilm size="3em" className={movieIconColor} onClick={() => { setMovieIconColor('movieIconSelected'); setTvIconColor('tvIcon'); }} />
+        <Link to="/" className={movieIconColor}>
+          <ImFilm size="3em" onClick={() => { setMovieIconColor('movieIconSelected'); setTvIconColor('tvIcon'); }} />
         </Link>
-        <Link to="/tvshows">
-          <ImTv size="3em" className={tvIconColor} onClick={() => { setTvIconColor('tvIconSelected'); setMovieIconColor('movieIcon'); }} />
+        <Link to="/tvshows" className={tvIconColor}>
+          <ImTv size="3em" onClick={() => { setTvIconColor('tvIconSelected'); setMovieIconColor('movieIcon'); }} />
         </Link>
       </div>
     </>
